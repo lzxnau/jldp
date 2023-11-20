835edaaa280d9023bf92b0da9358504014be25e2
lzxnau <lzxnau@gmail.com>
2023-11-20 16:49:28 +1100

    update

--- img/pack.py
+++ img/pack.py
@@ -1,66 +1,76 @@
 """
-Pip Package Upgrade Python Script
+Pip Package Upgrade Python Script.
 
+JLDP Project.
 :Author:  JLDP
 :Date:    20231119
-:Version: 1.0.1
+:Version: 1.0.2
 """
 
-pname = 'requirements.txt'
-uname = 'venv/up.txt'
+
+PNAME = "requirements.txt"
+UNAME = "venv/up.txt"
 plist = []
 
-def writeFx(d: dict) -> bool:
-  if len(d) == 0:
-    return False
 
-  with open(pname, 'r') as fr:
-    lines = fr.readlines()
-    for i, line in enumerate(lines):
-      line = line.strip(' \n')
-      if len(line) == 0:
-        continue
-      index = line.index('==')
-      key = line[0:index]
-      if key in d:
-        lines[i] = line.replace(line[index + 2:], d[key] + '\n')
+def write_fxn(d: dict) -> bool:
+    "Update requirements.txt."
+    if not d:
+        return False
 
-  with open(pname, 'w') as fw:
-    fw.writelines(lines)
+    with open(PNAME, "r", encoding="utf-8") as fr:
+        lines = fr.readlines()
+        for i, line in enumerate(lines):
+            line = line.strip(" \n")
+            if not line:
+                continue
+            index = line.index("==")
+            key = line[0:index]
+            if key in d:
+                lines[i] = line.replace(line[index + 2 :], d[key] + "\n")
 
-  return True
+    with open(PNAME, "w", encoding="utf-8") as fw:
+        fw.writelines(lines)
+
+    return True
+
 
 def check(lines: list) -> None:
-  fd = dict()
-  for line in lines:
-    wl = line.split()
-    if wl[0] in plist:
-      fd[wl[0]] = wl[2]
-
-  rt = writeFx(fd)
-
-  if rt == 0:
-    print('PIP_CHANGES=False')
-  else:
-    print('PIP_CHANGES=True')
-
-def getPList() -> None:
-  with open(pname, 'r') as f:
-    lines = f.readlines()
+    "Check if there are any update."
+    fd = {}
     for line in lines:
-      line = line.strip(' \n')
-      if len(line) == 0:
-        continue
-      index = line.index('==')
-      key = line[0:index]
-      plist.append(key)
+        wl = line.split()
+        if wl[0] in plist:
+            fd[wl[0]] = wl[2]
+
+    rt = write_fxn(fd)
+
+    if not rt:
+        print("PIP_CHANGES=False")
+    else:
+        print("PIP_CHANGES=True")
+
+
+def get_plist() -> None:
+    "Get the package list from requirements.txt."
+    with open(PNAME, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip(" \n")
+            if not line:
+                continue
+            index = line.index("==")
+            key = line[0:index]
+            plist.append(key)
+
 
 def main() -> None:
-  getPList()
-  with open(uname, 'r') as f:
-    lines = f.readlines()
-    check(lines)
+    "Main fxn for the pip package upgrade."
+    get_plist()
+    with open(UNAME, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        check(lines)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
