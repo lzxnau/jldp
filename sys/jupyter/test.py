74a6fe950e1a41fc4cdcadaabff21f05cb935177
lzxnau <lzxnau@gmail.com>
2023-11-27 23:26:31 +1100

    update

--- sys/jupyter/test.py
+++ sys/jupyter/test.py
@@ -1,6 +1,6 @@
 """Jupyter Lab Test Python Script."""
 
-import pytorch.examples.cv.ic.lions_cheetahs as lc
+import jlpy.pytorch.vision.image_classification.lions_cheetahs as lc
 
 lc.Explore.cnt_imgs()
 
