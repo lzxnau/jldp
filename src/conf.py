e98ea148e5ee7371443aa4e08d042524d356bfe9
lzxnau <lzxnau@gmail.com>
2023-12-06 23:08:53 +1100

    update

--- src/conf.py
+++ src/conf.py
@@ -71,7 +71,7 @@ myst_enable_extensions = [
 ]
 
 mermaid_output_format = "svg"
-mermaid_cmd = "mmdc --without-sandbox"
+mermaid_cmd = "mmdc"
 
 # autoclass_content = "both"
 autodoc_class_signature = "separated"
