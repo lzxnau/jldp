48ede7ac09bebf69377eedda3fe9eceb8bfd5898
lzxnau <lzxnau@gmail.com>
2023-12-06 15:53:42 +1100

    update

--- src/jlpy/pytorch/base/linear_regression/base_impl.py
+++ src/jlpy/pytorch/base/linear_regression/base_impl.py
@@ -18,34 +18,22 @@ class LRModel(nn.Module):
     .. card::
     """
 
-    def __init__(self, features: int = 10, sigma: float = 0.01) -> None:
+    def __init__(
+        self, features: int = 10, lr: float = 0.03, sigma: float = 0.01
+    ) -> None:
         """Construct a class instance."""
         super().__init__()
+        self.loss = nn.MSELoss()
+        self.lr = lr
         self.w: Tensor = torch.normal(
             0, sigma, (features, 1), requires_grad=True
         )
-        self.b: Tensor = torch.zeros(10, requires_grad=True)
-        print(self.b)
+        self.b: Tensor = torch.zeros(1, requires_grad=True)
 
     def forward(self, x: Tensor) -> Tensor:
         """Matrix x product with weights w plus bisa b."""
         y: Tensor = torch.matmul(x, self.w) + self.b
         return y
-
-    def loss(self, y: Tensor, y_hat: Tensor) -> Tensor:
-        """
-        Calculate the avaerage squared loss between real y and predicted y_hat.
-
-        :param y: Real y from dataset.
-        :type y: Tensor
-        :param y_hat: Predicted y_hat from model.
-        :type y_hat: Tensor
-        :return: The average squared loss.
-        :rtype: Tensor
-        """
-        sl: Tensor = (y_hat - y) ** 2 / 2
-        asl: Tensor = sl.mean()
-        return asl
 
 
 if __name__ == "__main__":
