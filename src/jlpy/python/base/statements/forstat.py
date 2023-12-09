bacab834cc812b4a3b60173c34f9d02d040788e2
lzxnau <lzxnau@gmail.com>
2023-12-09 12:28:58 +1100

    update

---
+++ src/jlpy/python/base/statements/forstat.py
@@ -0,0 +1,26 @@
+"""
+Python For Statement Test Module.
+
+.. card::
+* Iterable Object.
+^^^
+:__iter__():
+1. Return a iterator which implements __next__().
+2. Class implements the __next__().
+3. Work with yield which turns to a generator.
+4. All objects above are iterable objects.
+
+:__getitem__():
+1. Container holds a collection of iterable objects.
+2. Container implements this method for subscription. It also turns itself as \
+   a iterable object.
+
++++
+1. for item in iterable_object: loop through iterable object.
+2. for idx, item in enumerate(iterable_object): loop through iterable object \
+   with index.
+
+:Author:  JLDP
+:Version: 2023.12.09.1
+
+"""
