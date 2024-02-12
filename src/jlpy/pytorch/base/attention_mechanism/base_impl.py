27bcb0524fd61b7c037267aba0aaa13d0feeb541
lzxnau <lzxnau@gmail.com>
2024-02-12 16:41:19 +1100

    update

--- src/jlpy/pytorch/base/attention_mechanism/base_impl.py
+++ src/jlpy/pytorch/base/attention_mechanism/base_impl.py
@@ -7,3 +7,28 @@ Attention mechanism module for testing purposes.
 :Version: 2024.02.12.01
 
 """
+
+
+class AMBase:
+    """
+    Attention Mechanism Base Class.
+
+    .. card::
+    """
+
+    def __init__(self) -> None:
+        """
+        Construct a class instance.
+        """
+        pass
+
+    def show_heatmap(self, ts: Tensor) -> None:
+        """
+        Plot a heat map chart.
+
+        :param ts: Description.
+        :type ts: Tensor
+        :return: None
+        :rtype: None
+        """
+        print(ts)
