f646c3efbd06dde99a8adddb1019194243e1550b
lzxnau <lzxnau@gmail.com>
2024-02-16 12:20:55 +1100

    update

--- src/jlpy/pytorch/base/transformer/encoder.py
+++ src/jlpy/pytorch/base/transformer/encoder.py
@@ -7,3 +7,20 @@ Base implementation for a transformer's encoder.
 :Version: 2024.02.16.01
 
 """
+import torch
+import torch.nn as nn
+
+
+class EBlock(nn.modules):
+    """
+    Encoder Base Block for a RNN implementation.
+
+    Build a base RNN block for creating a transformer's encoder.
+
+    .. card::
+    """
+
+    def __init__(self) -> None:
+        """Construct a class instance."""
+        super().__init__()
+        pass
