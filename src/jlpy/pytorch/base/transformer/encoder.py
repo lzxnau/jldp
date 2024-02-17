ed96f28155155debe95dedf8e1ec07a59742dfa0
lzxnau <lzxnau@gmail.com>
2024-02-17 11:29:56 +1100

    update

--- src/jlpy/pytorch/base/transformer/encoder.py
+++ src/jlpy/pytorch/base/transformer/encoder.py
@@ -7,20 +7,40 @@ Base implementation for a transformer's encoder.
 :Version: 2024.02.16.01
 
 """
-import torch
-import torch.nn as nn
+from torch import nn
 
 
-class EBlock(nn.modules):
+class EBlock(nn.Module):
     """
-    Encoder Base Block for a RNN implementation.
+    Encoder base block for a RNN implementation.
 
     Build a base RNN block for creating a transformer's encoder.
 
     .. card::
     """
 
-    def __init__(self) -> None:
-        """Construct a class instance."""
+    def __init__(self, p1: None, p2: None) -> None:
+        """
+        Construct a class instance.
+
+        :param p1: Parameter 1.
+        :type p1: None
+        :param p2: Parameter 2.
+        :type p2: None
+        """
         super().__init__()
-        pass
+        self.p1 = p1
+        self.p2 = p2
+
+    def forward(self, p1: None, p2: None) -> None:
+        """
+        Override module's forward method.
+
+        :param p1: Parameter 1.
+        :type p1: None
+        :param p2: Parameter 2.
+        :type p2: None
+        :return: Return
+        :rtype: None
+        """
+        return None
